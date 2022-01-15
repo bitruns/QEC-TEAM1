bfab2c20d200542cbd66760ee019f9ea0d4f3e92
Andrew Fryer <andrew.fryer@queensu.ca>
2022-01-15 15:42:59 -0500

    backend passes tests

--- web_app/src/controllers/habitSuccess.js
+++ web_app/src/controllers/habitSuccess.js
@@ -1,15 +1,24 @@
 const { User } = require('../models/user');
 
 const create = async (req, res) => {
-  let newObj = {
-    ...req.body,
-  };
-  let newUser = new User(newObj);
+  const user = await User.findById(req.body.userId).exec();
+
+  for(const userHabit of user.habits) {
+    console.log(userHabit);
+    const habitId = userHabit.habitId.toString();
+    console.log('habitId: ' + habitId);
+    if(habitId == req.body.habitId) {
+      if(req.body.success) {
+        userHabit.numSuccess++;
+      }
+      userHabit.totalNum++;
+    }
+  }
 
   try {
-    await newUser.save();
+    await user.save();
     res.json({
-      user: newUser,
+      user,
     });
   } catch(e) {
     res.status(500).send(e);
